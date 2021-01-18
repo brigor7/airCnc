e77b419a37c30f44fa76b9c6646a4ba1aa4e7bad
=Bruno Igor <=brigor7@gmail.com>
2021-01-18 17:02:34 -0300

    Estilização de /Dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../connection';
+import './style.css';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
@@ -21,12 +23,13 @@ export default function Dashboard() {
       <ul className="spot-list">
         {spots.map((spot) => (
           <li key={spot._id}>
-            <header />
+            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
             <strong>{spot.company}</strong>
-            <span>{spot.price}</span>
+            <span>{spot.price ? `R$ ${spot.price}/dia` : 'GRATUITO'}</span>
           </li>
         ))}
       </ul>
+      <Link to="/new">Cadastrar novo spot</Link>
     </>
   );
 }
