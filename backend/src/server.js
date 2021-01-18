c4c9db757f8b3ac8c54d17843303a09ff570dd50
=Bruno Igor <=brigor7@gmail.com>
2021-01-18 16:25:04 -0300

    Criando acesso a um diretorio virtual

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const path = require('path');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+
 app.use(routes);
+
 const port = 3333;
 
 mongoose.connect(
@@ -15,5 +19,6 @@ mongoose.connect(
 );
 
 app.listen(port, () => {
+  console.log('dirname: ' + __dirname);
   console.log('✔Server Started! on port: ' + port);
 });
