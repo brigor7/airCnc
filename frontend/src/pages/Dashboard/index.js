235bbed3f3ab6b4486dab106c7d6b44cbbc14376
=Bruno Igor <=brigor7@gmail.com>
2021-01-18 16:02:39 -0300

    Trazendo informações /dashboard do back para front

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../connection';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const [spots, setSpots] = useState([]);
+  async function loadSpot() {
+    try {
+      const user_id = localStorage.getItem('user');
+      const response = await api.get('/dashboard', { headers: { user_id } });
+
+      setSpots(response.data);
+    } catch (error) {
+      console.log('Ocorreu um erro em /spot' + error);
+    }
+  }
+  useEffect(() => {
+    loadSpot();
+  }, []);
+  return (
+    <>
+      <ul className="spot-list">
+        {spots.map((spot) => (
+          <li key={spot._id}>
+            <header />
+            <strong>{spot.company}</strong>
+            <span>{spot.price}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
