818132c3afd1660316f016a053e93c53ae99e07a
=Bruno Igor <=brigor7@gmail.com>
2021-01-18 13:15:24 -0300

    Criado nova session acessando backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './connection';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      const newUser = await api.post('/sessions', email);
+      //localStorage('user_id', newUser.data._id);
+      console.log(newUser);
+    } catch (error) {
+      console.log('Erro ao realizar get em /session: ' + error);
+    }
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="logo" />
@@ -12,10 +25,15 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e<br />
           encontre <strong>talentos</strong> para sua empresa
         </p>
-        <form action="#">
+        <form onSubmit={handleLogin}>
           <label htmlFor="email">Email</label>
           <input type="text" id="email" placeholder="Seu melhor email" />
-          <button className="btn" type="submit">
+          <button
+            className="btn"
+            type="submit"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          >
             Cadastrar
           </button>
         </form>
