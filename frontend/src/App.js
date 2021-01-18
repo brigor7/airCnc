2a75c17e48adf0eeae5ef527e61e721fb3c67fe4
=Bruno Igor <=brigor7@gmail.com>
2021-01-18 11:54:07 -0300

    Estilização da página de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
 import './App.css';
+
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Ola Bruno, vamos começar o desenvolvimento do frontend do AirCnc</h1>
+    <div className="container">
+      <img src={logo} alt="logo" />
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e<br />
+          encontre <strong>talentos</strong> para sua empresa
+        </p>
+        <form action="#">
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" placeholder="Seu melhor email" />
+          <button className="btn" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
