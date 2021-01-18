4d4587d477dcf6beaefade19308b8e438a2e8165
=Bruno Igor <=brigor7@gmail.com>
2021-01-18 13:14:21 -0300

    Add cors and update port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+const port = 3333;
 
 mongoose.connect(
   'mongodb+srv://brigor_m0ng0:B@nc0_m0ng0@cluster0-fi27e.gcp.mongodb.net/aircnc?retryWrites=true&w=majority',
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
-app.listen(3000, () => {
-  console.log('✔Server Started!');
+app.listen(port, () => {
+  console.log('✔Server Started! on port: ' + port);
 });
