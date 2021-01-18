f4db8613bdb38064886842b7937ce106e19a269b
=Bruno Igor <=brigor7@gmail.com>
2021-01-18 17:20:02 -0300

    Create page new spot

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function () {
-  return <div />;
+export default function New() {
+  const [company, setCompany] = useState('');
+  const [price, setPrice] = useState(0);
+
+  function handleSubmit() {}
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="company">Empresa *</label>
+        <input
+          id="company"
+          placeholder="sua empresa incrivel"
+          type="text"
+          value={company}
+          onChange={(e) => {
+            setCompany(e.target.value);
+          }}
+        />
+        <label htmlFor="price">Preço *</label>
+        <input
+          id="price"
+          placeholder="seu valor por dia"
+          type="number"
+          value={price}
+          onChange={(e) => {
+            setPrice(e.target.value);
+          }}
+        />
+      </form>
+    </>
+  );
 }
