1ed7ef10de3fa427d32f394d87806b284898e680
=Bruno Igor <=brigor7@gmail.com>
2021-01-19 10:21:01 -0300

    Create page new spot

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 
 export default function New() {
   const [company, setCompany] = useState('');
-  const [price, setPrice] = useState(0);
+  const [price, setPrice] = useState('');
+  const [techs, setTechs] = useState('');
+  const [thumbnail, setThumbnail] = useState(0);
 
-  function handleSubmit() {}
+  function handleSubmit() {
+    console.log(company + ' ' + price + ' ' + thumbnail);
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -18,16 +23,42 @@ export default function New() {
             setCompany(e.target.value);
           }}
         />
-        <label htmlFor="price">Preço *</label>
+        <label htmlFor="techs">
+          Tecnologias* <span>(Separadas por virgula)</span>
+        </label>
+        <input
+          id="techs"
+          placeholder="Informe as tecnologias separadas por virgula"
+          type="text"
+          value={techs}
+          onChange={(e) => {
+            setTechs(e.target.value);
+          }}
+        />
+        <label htmlFor="price">
+          Valor diária* <span>(Em branco para gratuíto)</span>
+        </label>
         <input
           id="price"
-          placeholder="seu valor por dia"
-          type="number"
+          placeholder="Informe o valor da diária"
           value={price}
           onChange={(e) => {
             setPrice(e.target.value);
           }}
         />
+        <label htmlFor="thumbnail">Avatar *</label>
+        <input
+          id="thumbnail"
+          placeholder="Sua imagem"
+          type="text"
+          value={thumbnail}
+          onChange={(e) => {
+            setThumbnail(e.target.value);
+          }}
+        />
+        <button className="btn" type="submit">
+          Cadastrar
+        </button>
       </form>
     </>
   );
