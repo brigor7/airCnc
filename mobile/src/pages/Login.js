0558ef4ff7da7cf3cef8d245f4379bb5a84e8212
=Bruno Igor <=brigor7@gmail.com>
2021-01-22 16:14:17 -0300

    Create pages

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import {
+  View,
+  KeyboardAvoidingView,
+  AsyncStorage,
+  Plataform,
+  Image,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+
+import api from './../services/api';
+import logo from '../assets/logo.png';
+
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [techs, setTechs] = useState('');
+
+  async function handleSubmit() {
+    try {
+      const response = await api.post('/sessions', {
+        email,
+      });
+      const { message } = response.data;
+      const { _id } = message;
+      await AsyncStorage.setItem('user', _id);
+      await AsyncStorage.setItem('techs', techs);
+      navigation.navigate('List');
+    } catch (error) {
+      console.log('Erro ao criar session: ' + error);
+    }
+  }
+  return (
+    <KeyboardAvoidingView
+      enabled={Platform.OS === 'ios'}
+      behavior="padding"
+      style={styles.container}
+    >
+      <Image source={logo} />
+      <View style={styles.form}>
+        <Text style={styles.label}>SEU EMAIL *</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Seu email"
+          placeholderTextColor="#999"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={email}
+          onChangeText={setEmail}
+        />
+        <Text style={styles.label}>TECNOLOGIAS *</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Tecnologias de interesse"
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+          value={techs}
+          onChangeText={setTechs}
+        />
+        <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+          <Text style={styles.buttonText}>Encontrar spots</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  form: {
+    alignSelf: 'stretch',
+    paddingHorizontal: 30,
+    paddingTop: 30,
+  },
+  label: {
+    fontWeight: 'bold',
+    color: '#444',
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444',
+    height: 44,
+    marginBottom: 20,
+    borderRadius: 5,
+  },
+  button: {
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
